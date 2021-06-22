8d05ac50bdfa867591af8b3c8ec298ebea1cf333
Pierson <pro.pierson.alban@gmail.com>
2021-06-22 14:45:51 +0200

    WIP Notification

--- wwwroot/service-worker.js
+++ wwwroot/service-worker.js
@@ -3,3 +3,34 @@
 // be reflected on the first load after each change).
 console.log("service-worker");
 self.addEventListener('fetch', () => { });
+
+
+importScripts('https://www.gstatic.com/firebasejs/7.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.10.0/firebase-messaging.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyD24oaukrvdsx9lRqarKcg2SC7hV886vac",
+  projectId: "medieval-warfare",
+  messagingSenderId: "754418426163",
+  appId: "1:754418426163:web:3f222422be5ee60f5d9e46"
+});
+
+const messaging = firebase.messaging();
+messaging.usePublicVapidKey('BJlaPdeE3mUReTWUgbr6iLkaKfcQ4UsyXup0xVXKm9n1xBCEHb5G5rSyufNCmtaZiyiZq-26ZKWeQ92vD2rTzec');
+
+self.addEventListener('push', async event => {
+    const db = await getDb();
+    const tx = this.db.transaction('notifications', 'readwrite');
+    const store = tx.objectStore('notifications');
+  
+    const data = event.data.json().data;
+    data.id = parseInt(data.id);
+    store.put(data);
+  
+    tx.oncomplete = async e => {
+      const allClients = await clients.matchAll({ includeUncontrolled: true });
+      for (const client of allClients) {
+        client.postMessage('newData');
+      }
+    };
+  });
